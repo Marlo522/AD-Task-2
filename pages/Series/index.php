3b606ca96fae4f65d5dfa447612fbfedb059a4c4
Marlo522 <202311932@fit.edu.ph>
2025-05-17 15:54:18 +0800

    feat (Series page): add HTML structure to Series index page

--- pages/Series/index.php
+++ pages/Series/index.php
@@ -10,3 +10,17 @@ require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<?php
+head('Series');
+navHeader($navList);
+?>
+</body>
+</html>
