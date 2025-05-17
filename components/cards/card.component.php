68e3a299900943e6e72032b454ba1644c798acde
Marlo522 <202311932@fit.edu.ph>
2025-05-17 14:14:40 +0800

    fix (card component): correct image source path in renderCard function

--- components/cards/card.component.php
+++ components/cards/card.component.php
@@ -1,7 +1,7 @@
 <?php
 function renderCard($title, $image) {
     echo '<div class="card">';
-    echo '<img src="/assets/' . htmlspecialchars($image) . '" alt="' . htmlspecialchars($title) . '">';
+    echo '<img src="/assets/img/' . htmlspecialchars($image) . '" alt="' . htmlspecialchars($title) . '">';
     echo '<div class="card-title">' . htmlspecialchars($title) . '</div>';
     echo '</div>';
 }
