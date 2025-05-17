e33c0fded6685b820232fa0a97c4a6600dfc6936
Marlo522 <202311932@fit.edu.ph>
2025-05-17 15:49:20 +0800

    feat (Series page): add Series index page with necessary component imports

---
+++ pages/Series/index.php
@@ -0,0 +1,12 @@
+<?php
+require BASE_PATH . '/vendor/autoload.php';
+require BASE_PATH . '/utils/htmlEscape.utils.php';
+require BASE_PATH . '/utils/OutputAll.utils.php';
+
+require_once BASE_PATH . '/handlers/pageData.handler.php';
+require_once BASE_PATH . '/components/cards/note.component.php';
+require_once BASE_PATH . '/components/templates/foot.component.php';
+require_once BASE_PATH . '/components/templates/head.component.php';
+require_once BASE_PATH . '/components/templates/nav.component.php';
+
+?>
